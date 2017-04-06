29d568897c73a85ff58e7b8970e3f5fd1a735164
AnnaRomantsova <anutabur@mail.ru>
2017-04-06 13:42:27 +0300

    next

---
+++ src/main/java/SessionFactory.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Анюта on 06.04.2017.
+ */
+public class SessionFactory {
+}
---
+++ src/main/java/shop/goods.java
@@ -0,0 +1,34 @@
+package shop;
+
+/**
+ * Created by Анюта on 22.03.2017.
+ */
+public class goods {
+    private int id;
+    private int id_category;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_category() {
+        return id_category;
+    }
+
+    public void setId_category(int id_category) {
+        this.id_category = id_category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
